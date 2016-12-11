db77ab6561788e171b8ca8e16f2b65e8d13fdcf7
ywn <312369144@qq.com>
2016-12-11 19:33:37 +0800

    remove code used to synthetise sound

--- app/src/main/java/com/example/ywn/escapeproject/MainActivity.java
+++ app/src/main/java/com/example/ywn/escapeproject/MainActivity.java
@@ -16,10 +16,6 @@ import com.iflytek.cloud.SpeechError;
 import com.iflytek.cloud.SpeechUtility;
 import com.iflytek.cloud.ui.RecognizerDialog;
 import com.iflytek.cloud.ui.RecognizerDialogListener;
-import com.iflytek.cloud.ErrorCode;
-import com.iflytek.cloud.InitListener;
-import com.iflytek.cloud.SpeechSynthesizer;
-import com.iflytek.cloud.SynthesizerListener;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -28,92 +24,27 @@ import org.json.JSONTokener;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button btn_click;
-    private Button createSoundBtn;
-
     private EditText mResultText;
-    private SpeechSynthesizer mySynthesizer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btn_click = (Button) findViewById(R.id.startBtn);
-        createSoundBtn = (Button) findViewById(R.id.createVoiceBtn);
         mResultText = ((EditText) findViewById(R.id.contentText ));
 
         SpeechUtility.createUtility(this, SpeechConstant.APPID + "=584962eb");
 
         btn_click.setOnClickListener(this);
-        createSoundBtn.setOnClickListener(this);
-        mySynthesizer = SpeechSynthesizer.createSynthesizer(this, myInitListener);
     }
-
-    private InitListener myInitListener = new InitListener() {
-        @Override
-        public void onInit(int code) {
-            Log.v("mySynthesiezer:", "InitListener init() code = " + code);
-        }
-    };
 
     @Override
     public void onClick(View v) {
         if(v.getId() == R.id.startBtn)
                 btnVoice();
-        else if (v.getId() == R.id.createVoiceBtn)
-                createAiSound();
     }
 
-    private void createAiSound() {
-        // TODO Auto-generated method stub
-        //设置发音人
-        mySynthesizer.setParameter(SpeechConstant.VOICE_NAME,"xiaoyan");
-        //设置音调
-        mySynthesizer.setParameter(SpeechConstant.PITCH,"50");
-        //设置音量
-        mySynthesizer.setParameter(SpeechConstant.VOLUME,"50");
-        String str = mResultText.getText().toString();
-        int code = mySynthesizer.startSpeaking(str, mTtsListener);
-        Log.v("Syn start code:", code+"");
-    }
-    private SynthesizerListener mTtsListener = new SynthesizerListener() {
-        @Override
-        public void onSpeakBegin() {
-        }
-        @Override
-        public void onSpeakPaused() {
-        }
-        @Override
-        public void onSpeakResumed() {
-        }
-        @Override
-        public void onBufferProgress(int percent, int beginPos, int endPos, String info) {
-        }
-        @Override
-        public void onSpeakProgress(int percent, int beginPos, int endPos) {
-        }
 
-        @Override
-        public void onCompleted(SpeechError error) {
-            if(error!=null)
-            {
-                Log.v("Syn complete code:", error.getErrorCode()+"");
-            }
-            else
-            {
-                Log.v("Syn complete code:", "0");
-            }
-        }
-
-        @Override
-        public void onEvent(int eventType, int arg1, int arg2, Bundle obj) {
-            // 以下代码用于获取与云端的会话id，当业务出错时将会话id提供给技术支持人员，可用于查询会话日志，定位出错原因
-            // 若使用本地能力，会话id为null
-            //	if (SpeechEvent.EVENT_SESSION_ID == eventType) {
-            //		String sid = obj.getString(SpeechEvent.KEY_EVENT_SESSION_ID);
-            //		Log.d(TAG, "session id =" + sid);
-            //	}
-        }
-    };
 
     //TODO 开始说话：
     private void btnVoice() {
